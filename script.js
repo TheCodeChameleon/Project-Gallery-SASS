eb89aefdb1eba761ed8026b64758b4298224f0a9
TheCodeChameleon <thecodechameleon@gmail.com>
2023-04-27 19:56:46 -0300

    messageAlert added

---
+++ script.js
@@ -0,0 +1,7 @@
+// Display alert function
+function messageAlert(event) {
+  event.preventDefault();
+  alert(
+    "Normally this would take you to some webpage, but this is just a demo :)"
+  );
+}
